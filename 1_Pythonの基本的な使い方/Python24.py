ffaa2e93896eeb1f0fd32ef6cf76eb8938c28ba6
aocattleya <aocattleya@yahoo.co.jp>
2018-10-03 16:47:35 +0900

    基本的な使い方

--- 1_Pythonの基本的な使い方/Python24.py
+++ 1_Pythonの基本的な使い方/Python24.py
@@ -1,11 +1,22 @@
 # coding: UTF-8
+# お買い物代金を計算
 
 apple_price = 200
 
-input_count = input('購入するりんごの個数を入力してください: ')
+money = 1000
 
+input_count = input('購入するリンゴの個数を入力してください')
 count = int(input_count)
-total_price = apple_price * count
-
-print('購入するりんごの個数は' + str(count) + '個です')
+total_price = apple_price * input_count
+print('購入するリンゴの個数は' + str(count) + '個です')
 print('支払い金額は' + str(total_price) + '円です')
+
+if money > total_price:
+    print('リンゴを' + str(count) + '個買いました')
+    print('残高は' + str(money - total_price) + '円です')
+elif money == total_price:
+    print('リンゴ' + str(count) + '個買いました')
+    print('財布が空になりました')
+else:
+    print('お金が足りません')
+    print('リンゴを買えませんでした')
