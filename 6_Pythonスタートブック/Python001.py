3bd23a318986e58ff32302008d81ac1aef425b2b
aocattleya <aocattleya@yahoo.co.jp>
2018-10-17 20:36:39 +0900

    Pythonスタートブック

---
+++ 6_Pythonスタートブック/Python001.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+list_int = [0,1,2,3]
+print(list_int)
+
+list_mix = [2,1.732,'test']
+print(list_mix)
+
+print(list_mix[0])
+print(len(list_int))
+print(len(list_mix))
+
+# 要素の変更
+list_int[0] = -1
+print(list_int)
+
+# リストに新しい要素を追加する
+list_int.append(4)
+print(list_int)
+
+# 要素の削除
+list_int.pop(1)
+print(list_int)
+
+# リストの連結と拡張
+print(list_int + list_mix)
+
+# リストの連結と拡張（extend）
+list_int.extend(list_mix)
+print(list_int)
---
+++ 6_Pythonスタートブック/Python003.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+
+list_test = [4,9,3,-1,0]
+print(list_test)
+
+# 並び替え（小さい順）
+list_test.sort()
+print(list_test)
+
+# 並び替え（リストに格納されているデータの順序を逆さまにする）
+list_test.reverse()
+print(list_test)
+
+print(list_test)
