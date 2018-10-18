9e74f73c8b3d200462875539ee3e01981684f1c2
aocattleya <aocattleya@yahoo.co.jp>
2018-10-19 00:03:45 +0900

    Pythonスタートブック

---
+++ 6_Pythonスタートブック/Python004.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# 文字列の並び替え
+list_os = [ 'windows', 'mac', 'linux', 'BeOS' ]
+list_os.sort()
+print(list_os)
+
+# 大文字、小文字含む並び替え
+list_os = [ 'Windows', 'Mac', 'linux', 'BeOS' ]
+list_os.sort()
+print(list_os)
